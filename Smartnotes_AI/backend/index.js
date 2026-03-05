599c4624c112d63dba122951c453f39084a91e33
vaishnavee07 <vaishnaveem.csbs2024@citchennai.net>
2026-03-05 22:34:18 +0530

    Allow Vercel frontend in CORS

--- Smartnotes_AI/backend/index.js
+++ Smartnotes_AI/backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', ' https://smart-notes-ai-three-azure.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
