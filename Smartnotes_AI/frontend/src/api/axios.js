320a401cb473b00ba3d3184b4c4b481e9ac6f5f1
vaishnavee07 <vaishnaveem.csbs2024@citchennai.net>
2026-06-09 15:17:34 +0530

    fixing url

--- Smartnotes_AI/frontend/src/api/axios.js
+++ Smartnotes_AI/frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL =
     import.meta.env.MODE === 'development'
         ? 'http://localhost:5000/api'
-        : 'https://smartnotes-backend-5asd.onrender.com/api';
+        : 'https://smartnotes-backend-gdzc.onrender.com/api';
 
 const api = axios.create({
     baseURL: BASE_URL,
